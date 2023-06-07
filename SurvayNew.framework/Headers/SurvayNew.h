f451daca8667a4d9719a676c2c7e045ab5f0fa0f
technodroidz <79449782+Technodroidz@users.noreply.github.com>
2023-06-07 14:10:00 +0530

    Framework
    Framework

---
+++ SurvayNew.framework/Headers/SurvayNew.h
@@ -0,0 +1,18 @@
+//
+//  SurvayNew.h
+//  SurvayNew
+//
+//  Created by Gaurav Tomar on 17/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SurvayNew.
+FOUNDATION_EXPORT double SurvayNewVersionNumber;
+
+//! Project version string for SurvayNew.
+FOUNDATION_EXPORT const unsigned char SurvayNewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SurvayNew/PublicHeader.h>
+
+
